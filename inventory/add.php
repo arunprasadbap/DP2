4ee87aee8349f86ccf9c711946f5e0f9a033b435
dulitha60 <dulitha60@gmail.com>
2017-04-10 20:35:35 +0800

    Edited add php file and addInventory html file

--- inventory/add.php
+++ inventory/add.php
@@ -1,64 +1,63 @@
 <?php
-     // get post info
-    //$itemID    = $_POST['itemID'];
-        $itemName  = $_POST['itemname'];
-        $itemDesc  = $_POST['itemdec'];
-        $itemCount = $_POST['itemcount'];
-        $itemPrice = $_POST['itemprice'];
-        $count = count($itemId);
+if(isset($_POST['ItemName'])){
+    $itemName = $_POST['ItemName'];
+}
+if(isset($_POST['Itemdesc'])){
+    $Itemdec = $_POST['Itemdesc'];
+}
+if(isset($_POST['Itemcount'])){
+    $Itemcount = $_POST['Itemcount'];
+}
 
-    // authentication to the database
-        $servername = "localhost";
-        $username = "root";
-        $password = "rootroot";
+if (isset($_POST['Itemprice'])){
+    $Itemprice = $_POST['Itemprice'];
+}
+$servername ="localhost";
+$username="root";
+$password="rootroot";
 
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($servername,$username,$password);
 
-        // Check connection
-        if (!$conn) {
-            die("Connection failed: " . $conn->connect_error);
-        }else{
-                  
-                      // select database
+if(!$conn){
+    die("Connection failed: " . $conn->connect_error);
+    } else {
+       
         mysqli_select_db($conn, "dp2");
 
-        //Get next item ID from auto increment
+    
+     //Get next item ID from auto increment
         $sql = "SHOW TABLE STATUS WHERE `Name` = 'inventory';";
         $result = mysqli_query($conn, $sql);
         $data = mysqli_fetch_assoc($result);
 
         $itemId = $data['Auto_increment'];
 
-        // Add into Inventory table
-        $sql = "INSERT INTO `dp2` (itemId, itemname, itemcount, itemprice) VALUES($itemId, '$itemname', '$itemcount', '$itemprice')";
-                      
-             
-        if (mysqli_query($conn, $sql)) {
-            //Add item into Inventory Item table
-            $item_count = 0;
-            for ($i=0; $i<$count; $i++){
-                $sql = "INSERT INTO inventory (itemId, itemname, itemcount, itemprice) VALUES($itemId, '$itemname', '$itemcount', '$itemprice')";
-
-                if (mysqli_query($conn, $sql)){
-                    
-                    $item_count++;
+		$sqltable = "inventory";
+		
+		$querycom = "insert into $sqltable (item_name, itemdec, itemcount,itemprice) values ('$itemName', '$Itemdec','$Itemcount','$Itemprice')";
+		
+        if (mysqli_query($conn, $querycom)){
+                echo "ADDED";
+//                    $item_count++;
                 } else {
                     echo "Error: $sql <br />" . mysqli_error($conn);
                 }
             }
-       
-                if ($item_count == $count){
+
+            if ($item_count == $count){
                 echo "<div>
                 <script>
-                        window.alert('Record added successfully!');
-                        window.location.href = 'showall.php';
+                        window.alert(' added successfully!');
+                        window.location.href = 'addInventory.html';
                 </script>
                 </div>";
-            }
-        } else {
+            } else {
             echo "Error: $sql <br />" . mysqli_error($conn);
         }
-    }
     mysqli_close($conn);
 ?>
+  
+    
+    
+    
+}
