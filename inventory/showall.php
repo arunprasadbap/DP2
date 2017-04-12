3508b8a82113d032df227e3d26b2d0edf5a933ac
dulitha60 <dulitha60@gmail.com>
2017-04-12 18:44:00 +0800

    Created showall.php tp show the inventories in the table

---
+++ inventory/showall.php
@@ -0,0 +1,82 @@
+<?php
+
+$con = mysqli_connect("localhost","root","rootroot","dp2");
+$rs=mysqli_query($con,"SELECT * FROM inventory");
+$data=array();
+while($row=mysqli_fetch_assoc($rs)){
+    $data[]=$row;
+}
+
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset = "utf-8" />
+        <meta name = "viewport" content = "width = device - width, initial-scale=1" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link href="../style/style.css" rel="stylesheet"/> 
+        <script>
+        
+        var table = document.getElementById("tablee");
+        
+        
+        
+        
+        
+        </script>
+    <title>Inventory index</title>
+    </head>
+
+    <body>
+        <div class = "container">
+            <div>
+                <h1>People Health Pharmacy</h1>
+                <p id="home"></p>
+            </div>
+
+            <nav class="navbar navbar-default">
+              <div class="container-fluid">
+                <div class="navbar-header">
+                
+                </div>
+
+                <ul class="nav navbar-nav">
+                  <li><a href="../index.html">Home</a></li>
+                  <li class="active"><a href="../inventory/index.html">Inventory</a></li>
+                  <li><a href="../report/index.html">Report</a></li>
+                     <li><a href="../sales/index.html">Sales</a></li>
+                      <li><a href="../customers/index.html">Customers</a></li>
+                     
+                </ul>
+              </div>
+            </nav>
+    <div style="height:800px; overflow-y:auto;">
+		<table id="tablee" border="1em" class="table table-bordered table-striped table-hover">
+		  <thead>
+			<tr id="inventory">
+			<th id="inventory">Item Name</th>
+			<th id="inventory">Item Description</th>
+			<th id="inventory">Item Count</th>
+			<th id="inventory">Item Price</th>
+			<th id="inventory">Deletion</th>
+		</tr>
+		  </thead>
+
+		  <tbody>
+              <?php foreach($data as $inventory){?>
+              <tr>
+                  <td><?php echo $inventory['item_name'];?></td>
+                  <td><?php echo $inventory['itemdec'];?></td>
+                  <td><?php echo $inventory['itemcount'];?></td>
+                  <td><?php echo $inventory['itemprice'];?></td>
+                  
+              </tr>
+              <?php }?>
+              
+              
+		  </tbody>
+		</table>
+	</div>
+  </div>
+  </body>
+</html>
